5b934f2b4de8cbcf52f2342f492f285641d9c771
Asmir Mustafic <goetas@gmail.com>
2019-06-27 22:01:49 +0200

    leak test

--- leak.php
+++ leak.php
@@ -1,17 +1,10 @@
 #!/usr/bin/env php
 <?php
 
-set_time_limit(0);
-
 require __DIR__ . '/vendor/autoload.php';
-error_reporting(22527);
 
 $f = function () {
-    $files = new \Symfony\Component\Cache\Adapter\PhpFilesAdapter('C4wL-F1oiZ', 0, '/home/goetas/projects/leak/var/cache/prod/pools', 1);
-
-    $files->get('%5BApp%5CController%5CDefaultController%23leak%5D%5B1%5D', function (){
-
-    });
+    include 'cache.php';
 };
 
 foreach (range(1, 5000) as $i) {
