d34df2f298f29cab4467629ec67091b132ddae17
Asmir Mustafic <goetas@gmail.com>
2019-06-27 17:30:14 +0200

    add lock

--- leak.php
+++ leak.php
@@ -10,10 +10,10 @@ $f = function () {
     $files = new \Symfony\Component\Cache\Adapter\PhpFilesAdapter('C4wL-F1oiZ', 0, '/home/goetas/projects/leak/var/cache/prod/pools', 1);
     $array = new \Symfony\Component\Cache\Adapter\PhpArrayAdapter('/home/goetas/projects/leak/var/cache/prod/annotations.php', $files);
 
-    $preovider = new Symfony\Component\Cache\DoctrineProvider($array);
+    $provider = new Symfony\Component\Cache\DoctrineProvider($array);
 
     // The callable will only be executed on a cache miss.
-    $preovider->fetch( 'App\Controller\DefaultController#leak');
+    $provider->fetch( 'App\Controller\DefaultController#leak');
 };
 
 foreach (range(1, 5000) as $i) {
